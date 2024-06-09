e5f730bf577bc636d747e8c06484570db7e74fcd
bluesource <37376468+bluesourcecn@users.noreply.github.com>
2024-06-09 14:06:10 +0800

    新增使用流式分页查询方法

--- src/main/java/com/cn/mybatis/utils/SqlUtils.java
+++ src/main/java/com/cn/mybatis/utils/SqlUtils.java
@@ -8,6 +8,11 @@ import org.apache.commons.lang.StringUtils;
 import java.lang.invoke.SerializedLambda;
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.function.BiConsumer;
+import java.util.function.Function;
 
 @Slf4j
 public class SqlUtils {
@@ -92,6 +97,28 @@ public class SqlUtils {
             selectColumn.append(toColumn(fn)).append(",");
         }
         return selectColumn.toString().replaceAll(",$", "");
+    }
+
+    /**
+     * 使用流式分页查询
+     *
+     * @param condition 查询条件, 一般为主键或某些字段 在sql中 in
+     * @param biConsumer 执行查询操作
+     * @return 查询结果
+     * @param <T> 查询条件的类型, 比如主键可以为{@link String}或者{@link Long}
+     * @param <R> 查询结果类型
+     */
+    public static <T, R> List<R> queryPage(List<T> condition, BiConsumer<List<T>, List<R>> biConsumer) {
+        log.info("[queryPage]size={}", condition.size());
+        List<R> container = new ArrayList<>();
+        List<List<T>> splitList = CollUtil.split(condition, 960);
+        for (int i = 0; i < splitList.size(); i++) {
+            List<T> idSplit = splitList.get(i);
+            biConsumer.accept(idSplit, container);
+            log.info("[queryPage]查询进度{}/{}", i + 1, splitList.size());
+        }
+        log.info("[queryPage]end list.size=" + container.size());
+        return container;
     }
 
 }
