7320a32fd45261efe68ae493c9554a56082aa410
bluesource <37376468+bluesourcecn@users.noreply.github.com>
2025-08-16 10:19:16 +0800

    Update MybatisUtil.java
    Optimized the code structure and added exception handling

--- src/main/java/com/cn/mybatis/utils/MybatisUtil.java
+++ src/main/java/com/cn/mybatis/utils/MybatisUtil.java
@@ -1,44 +1,53 @@
 package com.cn.mybatis.utils;
 
+import cn.hutool.core.collection.CollUtil;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.ibatis.session.ExecutorType;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
-import java.util.function.ToIntBiFunction;
 
 @Slf4j
 @Component
 @Order(Integer.MAX_VALUE)
 public class MybatisUtil {
-    @Autowired
-    private SqlSessionFactory sqlSessionFactory;
+    private static SqlSessionFactory sqlSessionFactory;
 
-    /**
-     * mybatis 批量操作
+    public MybatisUtil(SqlSessionFactory sqlSessionFactory) {
+        MybatisUtil.sqlSessionFactory = sqlSessionFactory;
+    }
+
+     /**
+     * mybatis-plus 批量入库
      *
-     * @param list 需要插入数据库的实体对象集合
-     * @param mapperClass mapper
-     * @param mapperRToIntBiFunction 指定单条插入方法,必须是单条插入
+     * @param list 需要入库的集合
+     * @param mapperClass 对应的Mapper的class
+     * @param <M> Mapper
+     * @param <T> Entity
      */
-    public <T, Mapper> void batchInsert(List<T> list, Class<Mapper> mapperClass, ToIntBiFunction<Mapper, T> mapperRToIntBiFunction) {
-        //切换batch模式
-        SqlSession sqlSession = sqlSessionFactory.openSession(ExecutorType.BATCH);
-        try {
-            Mapper mapper = sqlSession.getMapper(mapperClass);
-            for (T item : list) {
-                //调用方法
-                mapperRToIntBiFunction.applyAsInt(mapper, item);
+    public static <M extends BaseMapper<T>, T> void insertBatch(List<T> list, Class<M> mapperClass) {
+        if (CollUtil.isEmpty(list)) {
+            return;
+        }
+        int size = list.size();
+        try (SqlSession sqlSession = sqlSessionFactory.openSession(ExecutorType.BATCH, false)) {
+            M mapper = sqlSession.getMapper(mapperClass);
+            try {
+                for (int i = 0; i < size; i++) {
+                    mapper.insert(list.get(i));
+                    if (i > 0 && ((i % 1000 == 0) || i == size - 1)) {
+                        sqlSession.flushStatements();
+                    }
+                }
+                sqlSession.commit();
+            } catch (Exception e) {
+                sqlSession.rollback();
+                throw new RuntimeException("批量入库失败,总数:" + size, e);
             }
-            sqlSession.commit();//提交事务
-        } catch (Exception e) {
-            log.error("[batchInsert]批量插入报错", e);
-        } finally {
-            sqlSession.close();
         }
     }
 }
